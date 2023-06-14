a4cd66f12fee69447d0689c579473521a45ef185
safym <osafym@gmail.com>
2023-06-14 23:44:45 +0300

    task 7: add todo form component

---
+++ src/app/components/todo-form/todo-form.component.ts
@@ -0,0 +1,27 @@
+import { Component, EventEmitter, Output } from "@angular/core";
+import { Status, Todo } from "src/app/models/todo";
+import { TodoItem } from "src/app/todo";
+
+@Component({
+  selector: "app-todo-form",
+  templateUrl: "./todo-form.component.html",
+})
+export class TodoFormComponent {
+  title: string = "";
+  status: Status = "normal";
+
+  @Output() addTodoEvent = new EventEmitter<Todo>();
+
+  changeStatus(e: any) {
+    this.status = e.target.value;
+  }
+
+  onSubmit() {
+    const newTodo = new TodoItem(Date.now(), this.title, this.status);
+
+    this.addTodoEvent.emit(newTodo);
+
+    this.title = "";
+    this.status = "normal";
+  }
+}
