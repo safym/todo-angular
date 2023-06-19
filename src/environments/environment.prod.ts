a0d0b63fdd840b5279a386e3b2d8fa5260861ad3
safym <osafym@gmail.com>
2023-06-20 00:25:03 +0300

    add environment

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+import { EnvironmentInterface } from "./environment-interface";
+
+export const environment: EnvironmentInterface = {
+  production: true,
+  baseUrl: 'https://api.fit-meetups.ru/',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+import { EnvironmentInterface } from "./environment-interface";
+
+export const environment: EnvironmentInterface = {
+  production: false,
+  baseUrl: 'https://api.fit-meetups.ru/',
+};
