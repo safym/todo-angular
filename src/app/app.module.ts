aeaaa24c4d59e2ac03777fbdb40d99c1be2142db
safym <osafym@gmail.com>
2023-06-20 00:26:20 +0300

    fix app module declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TodoItemComponent } from './components/todo-item/todo-item.component';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
 import { TodoFormComponent } from './components/todo-form/todo-form.component';
-import { FormsModule } from '@angular/forms';
 import { FilterFormComponent } from './components/filter-form/filter-form.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+
+import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './components/header/header.component';
+import { TodoPageComponent } from './pages/todo-page/todo-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TodoItemComponent,
-    TodoListComponent,
-    TodoFormComponent,
-    FilterFormComponent
-  ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    TodoPageComponent,
+    LoginPageComponent,
+    AboutPageComponent,
+    LoginFormComponent,
+    TodoFormComponent,
+    FilterFormComponent,
+    TodoItemComponent,
+    TodoListComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
