bb39ab476519834264ee3578ed155ef37adca693
safym <osafym@gmail.com>
2023-06-21 19:40:09 +0300

    add token interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,27 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
 
-import { AppComponent } from './app.component';
-import { TodoItemComponent } from './components/todo-item/todo-item.component';
-import { TodoListComponent } from './components/todo-list/todo-list.component';
-import { TodoFormComponent } from './components/todo-form/todo-form.component';
-import { FilterFormComponent } from './components/filter-form/filter-form.component';
-import { LoginFormComponent } from './components/login-form/login-form.component';
+import { AppComponent } from "./app.component";
+import { TodoItemComponent } from "./components/todo-item/todo-item.component";
+import { TodoListComponent } from "./components/todo-list/todo-list.component";
+import { TodoFormComponent } from "./components/todo-form/todo-form.component";
+import { FilterFormComponent } from "./components/filter-form/filter-form.component";
+import { LoginFormComponent } from "./components/login-form/login-form.component";
 
-import { AppRoutingModule } from './app-routing.module';
-import { HeaderComponent } from './components/header/header.component';
-import { TodoPageComponent } from './pages/todo-page/todo-page.component';
-import { LoginPageComponent } from './pages/login-page/login-page.component';
-import { AboutPageComponent } from './pages/about-page/about-page.component';
-import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { HeaderComponent } from "./components/header/header.component";
+import { TodoPageComponent } from "./pages/todo-page/todo-page.component";
+import { LoginPageComponent } from "./pages/login-page/login-page.component";
+import { AboutPageComponent } from "./pages/about-page/about-page.component";
+import { NotFoundPageComponent } from "./pages/not-found-page/not-found-page.component";
+import { TokenInterceptor } from "./interceptors/token.interceptor";
+import { MeetupPageComponent } from "./pages/meetup-page/meetup-page.component";
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpClientModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, FormsModule, HttpClientModule, AppRoutingModule],
   declarations: [
     AppComponent,
     HeaderComponent,
@@ -37,8 +34,15 @@ import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.com
     TodoItemComponent,
     TodoListComponent,
     NotFoundPageComponent,
+    MeetupPageComponent,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true,
+    },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
